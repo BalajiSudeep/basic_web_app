5384bdbea3295fc7b3ac2a29ea2ddaa2df559f62
BalajiSudeep <balajisudeep1@gmail.com>
2025-05-10 09:13:43 +0000

    modified app.py for all servers

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-# app.py
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
-# Home route that displays a simple form
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-# Route to handle form submission
-@app.route('/submit', methods=['POST'])
-def submit():
-    name = request.form['name']
-    message = f"Hello, {name}! Thank you for submitting the form."
-    return render_template('result.html', message=message)
+    return "Hello from Flask App running via Jenkins CI/CD Pipeline!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 

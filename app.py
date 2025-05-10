be2a558804798f4193363447bc3aa4c4a124e724
BalajiSudeep <93464642+BalajiSudeep@users.noreply.github.com>
2025-05-10 15:37:07 +0530

    Update app.py to debug off

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask App running via Jenkins CI/CD Pipeline!"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
 
